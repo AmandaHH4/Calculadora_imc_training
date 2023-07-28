5ecb178bb9519ef5c6137436411c34ede58bb8f4
amanda <amandahhhimler4@gmail.com>
2023-07-27 21:44:49 -0300

    training

---
+++ Calculadora Imc/script.js
@@ -0,0 +1,185 @@
+// Algoritimo
+
+// calcular IMC
+// 1 Pegar valores
+// 2 Calcular o Imc
+// 3 Gerar classificacao do imc 
+// 4 Organizar as informacoes 
+// 5 Salvar os dados na lista 
+// 6 ler a lista com os dados 
+// 7 renderizar conteudo no html 
+// 8 botao de limpar os registros 
+
+// funcao principal
+
+function calcularImc(event) {
+   // event.preventDefault()
+    console.log("Funcionante!!!");
+
+    pegarValores();
+
+    let dadosUsuario = pegarValores ();
+
+    let imc = calcular(dadosUsuario.altura, dadosUsuario.peso);
+
+    //console.log(ClassificarImc (imc));
+
+    let classificacao = ClassificarImc(imc);
+
+    let usuarioAtualizado = organizarDados(dadosUsuario, imc, classificacao)
+
+    cadastrarUsuario(usuarioAtualizado);
+
+
+}
+
+// Passo 1 pegar valor 
+function pegarValores() {
+    let nomeRecebido = document.getElementById("nome").value.trim();
+    let alturaRecebida = parseFloat(document.getElementById("altura").value);
+    let pesoRecebido = parseFloat(document.getElementById("peso").value);  
+
+    let dadosUsuario = {
+        nome: nomeRecebido,
+        altura: alturaRecebida,
+        peso: pesoRecebido,
+    }
+
+    console.log(dadosUsuario);
+
+    return dadosUsuario;
+}
+
+
+// Passo 2 calcular 
+
+function calcular(altura, peso) {
+    let imc = peso/ (altura * altura) 
+
+    console.log(imc);
+
+    return imc; 
+}
+
+// Passo 3 Classificar 
+
+function ClassificarImc(imc) {
+    /*
+    resultado             situacao 
+    menor que 18,5         Filezinho
+    entre 18,5 e 24,99     dilica
+    entre 25 e 29,99       ta top
+    acima de 30           Oh la em casa 
+    */
+
+  if(imc < 18,5) {
+    return "Filezinho!!!";
+  }
+
+  else if(imc < 25) {
+    return "Dilica!!"
+    
+  }
+  else if(imc < 30) {
+    return "ta top!!" 
+  }
+  else{
+    return "Oh la em casa"
+  }
+
+}
+
+// Passo 4 Organizar informacoes 
+function organizarDados(dadosUsuario, valorImc, ClassificarImc) {
+  let dataHoraAtual = Intl.DateTimeFormat('pt-BR', {timeStyle: 'long', dateStyle: 
+  'short'}).format(Date.now());
+
+  let dadosUsuarioAtualizado ={
+    ...dadosUsuario,
+    imc: valorImc.toFixed(2),
+    classificacao: ClassificarImc,
+    dataCadastro: dataHoraAtual
+  }
+  
+  console.log( dadosUsuarioAtualizado);
+
+  return dadosUsuarioAtualizado
+
+}
+
+// 5 Salvar 
+
+function cadastrarUsuario(usuario) {
+  let listaUsuarios = [];
+
+
+  // if (localStorage.getItem("usuariosCadastrados") == true ) { 
+  //}
+  if (localStorage.getItem("usuariosCadastrados") ) {
+    listaUsuarios = JSON.parse(localStorage.getItem("usuariosCadastrados"));
+  }
+
+  listaUsuarios.push(usuario)
+  localStorage.setItem("usuariosCadastrados", JSON.stringify(listaUsuarios))
+}
+
+// 6 ler lista 
+function carregarUsuarios() {
+  let listaUsuarios = [];
+
+  if (localStorage.getItem("usuariosCadastrados") ) {
+    listaUsuarios = JSON.parse(localStorage.getItem("usuariosCadastrados"));
+  }
+
+
+
+if (listaUsuarios.length == 0 ) {
+  let tabela = document.getElementById("corpo-tabela");
+
+  tabela.innerHTML = `<tr class="linha-mensagem">
+  <td colspan="6">
+      Nenhum usuario cadastrado !
+  </td>
+  </tr>`
+}else{
+  montarTabela(listaUsuarios)
+}
+
+}
+
+
+window.addEventListener('DOMContentLoaded', () => carregarUsuarios())
+
+// passo 7 montar a tabela 
+
+
+function montarTabela(listaDeCadastrados) {
+  let tabela = document.getElementById("corpo-tabela");
+
+  let template = '';
+
+  listaDeCadastrados.forEach(pessoa => {
+   template += 
+    `<tr>
+       <td data-cell="nome">${pessoa.nome}</td>
+       <td data-cell="altura">${pessoa.altura}</td>
+       <td data-cell="peso">${pessoa.peso}</td>
+       <td data-cell="valor do IMC">${pessoa.imc}</td>
+       <td data-cell="classificação do IMC">${pessoa.classificacao}</td>
+       <td data-cell="data de cadastro">${pessoa.dataCadastro}</td>
+   </tr>`  
+  });
+
+  tabela.innerHTML = template;
+
+
+
+}
+
+
+// Passo 8 limpar local storage
+function deletarRegistros() {
+  localStorage.removeItem("usuariosCadastrados")
+  window.location.reload();
+
+}
